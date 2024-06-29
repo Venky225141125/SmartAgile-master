97368c211c38c5555ffbe6172d1ec26b643b6133
Saisree5 <98836959+Saisree5@users.noreply.github.com>
2024-06-29 14:29:07 +0530

    Update logout in urls.py

--- SmartAgile-master/backend/smartagile/urls.py
+++ SmartAgile-master/backend/smartagile/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('check_data/', CheckDataView.as_view(), name='check_data'),
     path('forgetpassword/', ForgotPasswordView.as_view(), name='forgetpassword'),
     path('resetpassword/', ResetPasswordView.as_view(), name='resetpassword'),
+    path('logout/',logout_view,name="logout")
 ]
