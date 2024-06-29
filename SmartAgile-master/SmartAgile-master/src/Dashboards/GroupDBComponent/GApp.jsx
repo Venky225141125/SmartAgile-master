41fe216d1761c9f8081ea75cf082285b46be8224
Saisree5 <98836959+Saisree5@users.noreply.github.com>
2024-06-29 14:21:50 +0530

    Add files via upload

---
+++ SmartAgile-master/SmartAgile-master/src/Dashboards/GroupDBComponent/GApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GroupDashboard from './components/GroupDBComponent/GroupDashboard';
+import GHome from './components/GroupDBComponent/GHome';
+import './index.css';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/*" element={<GroupDashboard />}>
+        <Route path="ghome" element={<GHome />} />
+      </Route>
+    </Routes>
+  </Router>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
